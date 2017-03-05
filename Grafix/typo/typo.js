465c7145ce86f042d14b058688cb5a224fceec86
Marc A. Donis <mad7@runbox.com>
2017-03-05 11:39:03 +0100

    v1.4
    omg localStorage!!!

--- Grafix/typo/typo.js
+++ Grafix/typo/typo.js
@@ -1,7 +1,49 @@
+var config = {
+	maxTime : 40, // secs	
+	newBallProbability : 0.025,
+	levels : [
+		{
+			name : "Beginner",
+			gravity : 0.00001,
+			grams : [ "a", "s", "d", "f", "j", "k", "l", "g", "h" ],
+		},
+		{
+			name : "Level 2",
+			gravity : 0.000012,
+			grams : [ "aq", "sw", "de", "fr", "ju", "ki", "lo", "p", "ft", "jh", "fg" ],
+		},
+		{
+			name : "Level 3",
+			gravity : 0.000012,
+			grams : [ "ay", "sx", "dc", "fv", "gv", "jn", "jm", "k,", "l.", "jh", "fg" ],
+		},
+		{
+			name : "Intermediate",
+			gravity : 0.000015,
+			grams : [ "papa", "haha", "lol", "mama", "rar", "dada", "fifi", "fofo", "nana", "popo", "tata", "toto", "fyfy", "gogo", "gaga" ],
+		},
+		{
+			name : "Advanced",
+			gravity : 0.00002,
+			grams : [ "jun", "jul", "may", "jan", "ver", "sew", "wet", "pol", "nop", "fre", "nuh", "vop", "dee", "boo", "oop", "bin", "hex", "dec",
+				"ibm", "ocr", "fra", "usa", "the", "une", "dog", "cat", "big", "pig", "sun", "eat", "dot", "dig", "pup", "hen", "vat", "ici", "moi",
+				"ton", "nos", "him", "his", "her", "she", "out", "our" ],
+		},
+		{
+			name : "Hardcore",
+			gravity : 0.000025,
+			grams : [ "the", "quick", "brown", "fox", "jumped", "over", "lazy", "kangaroo", "dog", "monkey", "duck", "jump", "troll", "giant",
+				"zulu", "boat", "ship", "work", "play", "rain", "cloud", "west", "north", "south", "east", "atom", "quark", "mouse", "bunny" ],
+		},
+		{
+			name : "Ludicrous",
+			gravity : 0.00006,
+			grams : [ "this", "here", "omg", "wtf", "madness", "wow", "r5zq", "please", "stop", "321go", "words", "fast", "very", "8px32", "dd6gv",
+				"bq992" ],
+		}, ]
+};
+
 var interval = 25; // ms
-var maxTime = 2 * 60 * 1000 / interval;
-var gravity = 0.00002;
-var grams;
 var balls = [];
 var t = 0;
 var typedKeys = [];
@@ -12,20 +54,41 @@ var score = 0;
 var hiScore = 0;
 var mainInterval;
 var level = 0;
+var isGameOver = false;
+var maxSteps;
 
-var badKeySound = new Audio("audio/Loud-train-horn.wav");
-var ballAtBottomSound = new Audio("audio/ball-at-bottom.wav");
-var keyPressSound = new Audio("audio/Button-click-sound.wav");
-var ballCompleteSound = new Audio("audio/Spinning-sound.wav");
+var badKeySound = new Audio("audio/Funny-noise.mp3");
+var ballAtBottomSound = new Audio("audio/ball-at-bottom.mp3");
+var keyPressSound = new Audio("audio/Button-click-sound.mp3");
+var ballCompleteSound = new Audio("audio/Spinning-sound.mp3");
 
 function onLoad() {
 	gameCanvas = document.getElementById("game-canvas");
 	gameContext = gameCanvas.getContext("2d");
 	progressBar = document.getElementById("game-progress");
+
+	setup();
 	selectLevel(2);
 }
 
+function setup() {
+	var html = "<ul>";
+	for ( var i in config.levels) {
+		html += "<li><a href=\"#\" id=\"level-" + i + "\" onClick=\"selectLevel(" + i + ");\">" + config.levels[i].name + "</a></li>";
+	}
+	html += "</ul>";
+	var levelSelectionDiv = document.getElementById("level-selection");
+	levelSelectionDiv.innerHTML = html;
+
+	var storedConfig = localStorage.getItem("config");
+	if (storedConfig) {
+		config = JSON.parse(storedConfig);
+	}
+}
+
 function start() {
+	maxSteps = config.maxTime * (1000 / interval);
+	isGameOver = false;
 	isPaused = false;
 	typedKeys = [];
 	balls = [];
@@ -46,20 +109,21 @@ function step() {
 		return;
 	}
 	t++;
-	if (t > maxTime) {
+	if (t > maxSteps) {
 		clearInterval(mainInterval);
 
 		isPaused = true;
+		isGameOver = true;
 		hiScore = Math.max(hiScore, score);
-		document.getElementById("hi-score").innerHTML = hiScore;
+		document.getElementById("hi-score").innerHTML = Math.round(hiScore);
 
 		setMessage("GAME OVER");
 		return;
 	}
 
-	progressBar.style.width = (100 * t / maxTime) + "%";
+	progressBar.style.width = (100 * t / maxSteps) + "%";
 
-	if (Math.random() < 0.025) {
+	if (Math.random() < config.newBallProbability) {
 		var ball = {
 			y : 0,
 			x : Math.random(),
@@ -67,13 +131,15 @@ function step() {
 				x : (Math.random() - 0.5) * 0.01,
 				y : Math.random() * 0.0012
 			},
-			gram : grams[parseInt(Math.random() * grams.length)],
+			gram : config.levels[level].grams[parseInt(Math.random() * config.levels[level].grams.length)],
 			color : hsvToRgb(Math.random(), 0.4, 0.8),
 			radius : 25 + (Math.random() * 15),
 			radiusFreq : 8 + (Math.random() * 5),
 			radiusAmplitude : 4 + (Math.random() * 2),
 			typedLettersIndex : -1,
-			id : currId++
+			id : currId++,
+			isComplete : false,
+			fontSize : 28
 		};
 		balls.push(ball);
 	}
@@ -88,20 +154,31 @@ function step() {
 	var toRemove = [];
 	for ( var i in balls) {
 		var ball = balls[i];
-		ball.vel.y += gravity;
-		ball.y += ball.vel.y;
-		ball.x += ball.vel.x;
-		var radius = ball.radius + (Math.sin(t / ball.radiusFreq) * ball.radiusAmplitude);
-		if (ball.x < 0) {
-			ball.x *= -1;
-			ball.vel.x *= -1;
-		} else if (ball.x >= 1) {
-			ball.x = 2 - ball.x;
-			ball.vel.x *= -1;
+		if (ball.isComplete) {
+			ball.radius *= 0.9;
+			ball.fontSize--;
+			ball.fontSize = Math.max(1, ball.fontSize);
+			if (ball.radius < 0.05) {
+				toRemove.push(ball);
+			}
+		} else {
+			ball.vel.y += config.levels[level].gravity;
+			ball.y += ball.vel.y;
+			ball.x += ball.vel.x;
+			if (ball.x < 0) {
+				ball.x *= -1;
+				ball.vel.x *= -1;
+			} else if (ball.x >= 1) {
+				ball.x = 2 - ball.x;
+				ball.vel.x *= -1;
+			}
+			if (ball.y > 1) {
+				toRemove.push(ball);
+				addScore(-40);
+				ballAtBottomSound.play();
+			}
 		}
-		if (ball.y > 1) {
-			toRemove.push(ball);
-		}
+		var radius = Math.max(0, ball.radius + (Math.sin(t / ball.radiusFreq) * ball.radiusAmplitude));
 
 		var x = ball.x * gameCanvas.width;
 		var y = ball.y * gameCanvas.height + radius;
@@ -117,7 +194,7 @@ function step() {
 		gameContext.arc(x, y, radius, 0, 2 * Math.PI);
 		gameContext.fill();
 
-		gameContext.font = "bold 28px Arial";
+		gameContext.font = "bold " + ball.fontSize + "px Arial";
 		gameContext.textBaseline = "middle";
 		var typedLetters = ball.gram.substring(0, ball.typedLettersIndex + 1);
 		var untypedLetters = ball.gram.substring(ball.typedLettersIndex + 1);
@@ -133,11 +210,7 @@ function step() {
 		gameContext.fillText(untypedLetters, x - halfWidth + typedLettersWidth, y);
 	}
 
-	if (toRemove.length > 0) {
-		addScore(-40);
-		ballAtBottomSound.play();
-		removeBalls(toRemove);
-	}
+	removeBalls(toRemove);
 }
 
 function removeBalls(toRemove) {
@@ -163,29 +236,57 @@ function onClick(e) {
 			togglePause();
 		} else if (e.target.id == "start-button") {
 			start();
+		} else if (e.target.id == "edit-config") {
+			isPaused = true;
+			document.getElementById("config").style.display = "block";
+			document.getElementById("config-text").value = JSON.stringify(config, null, "\t") + "\n";
+		} else if (e.target.id == "save-config") {
+			var configStr = document.getElementById("config-text").value;
+			try {
+				config = JSON.parse(configStr);
+			} catch (err) {
+				alert("Failed to parse configuration!  Check again.");
+				return;
+			}
+			document.getElementById("config").style.display = "none";
+			localStorage.setItem("config", configStr);
+			setup();
+			selectLevel(level);
+			start();
+		} else if (e.target.id == "cancel-config") {
+			document.getElementById("config").style.display = "none";
 		}
 	}
 	//	console.log(JSON.stringify(e.target.id));
 }
 
 function onKeyPress(e) {
-	if (e.key == " ") {
-		togglePause();
+	var key = e.key.toLowerCase();
+	if (key == " ") {
+		if (isGameOver) {
+			start();
+		} else {
+			togglePause();
+		}
 		return;
 	}
 	if (isPaused) {
 		return;
 	}
-	typedKeys.push(e.key);
+	typedKeys.push(key);
 
 	var completedBalls = [];
 	var isFound = false;
 	for ( var i in balls) {
 		var ball = balls[i];
-		if (ball.gram.charAt(ball.typedLettersIndex + 1) == e.key) {
+		if (ball.isComplete) {
+			continue;
+		}
+		if (ball.gram.charAt(ball.typedLettersIndex + 1) == key) {
 			ball.typedLettersIndex++;
 			isFound = true;
 			if (ball.typedLettersIndex + 1 == ball.gram.length) {
+				ball.isComplete = true;
 				completedBalls.push(ball);
 			}
 			//			console.log(ball.gram + " " + ball.typedLettersIndex);
@@ -205,7 +306,7 @@ function onKeyPress(e) {
 			var ball = completedBalls[i];
 			addScore(40 * (1 - ball.y) * ball.gram.length);
 		}
-		removeBalls(completedBalls);
+		//		removeBalls(completedBalls);
 		ballCompleteSound.play();
 	}
 }
@@ -223,27 +324,6 @@ function selectLevel(l) {
 	document.getElementById("level-" + level).className = "";
 	level = l;
 	document.getElementById("level-" + level).className = "selected";
-
-	if (level == 0) {
-		gravity = 0.00001;
-		grams = [ "a", "s", "d", "f", "j", "k", "l" ];
-	} else if (level == 1) {
-		gravity = 0.000012;
-		grams = [ "as", "jk", "da", "df", "a", "la", "ga", "gh" ];
-	} else if (level == 2) {
-		gravity = 0.00002;
-		grams = [ "juju", "frf", "ded", "kik", "r", "e", "sas", "lala", "sese", "fd" ];
-	} else if (level == 3) {
-		gravity = 0.000025;
-		grams = [ "go", "this", "ded", "kik", "r", "e", "sas", "lala", "sese", "fd" ];
-	} else if (level == 4) {
-		gravity = 0.00003;
-		grams = [ "juju", "frf", "ded", "kik", "r", "e", "sas", "lala", "sese", "fd" ];
-	} else if (level == 5) {
-		gravity = 0.00006;
-		grams = [ "this", "here", "omg", "wtf", "madness", "wow", "r5zq", "please", "stop", "321go", "words", "fast", "very", "8px32", "dd6gv",
-			"bq992" ];
-	}
 	start();
 }
 
